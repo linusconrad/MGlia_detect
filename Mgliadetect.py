0916e7486787ec1f5d4ac93bc4fe81bac0fac19f
Linus Conrad <l.conrad@sheffield.ac.uk>
2022-06-13 16:29:16 +0100

    moved the mian loop to the script, started adding a second round detection to the notebook

--- Mgliadetect.py
+++ Mgliadetect.py
@@ -8,8 +8,6 @@ import cupyx.scipy.ndimage as ndi
 
 import seaborn as sns
 import pandas as pd
-# Set matplotlib backend
-%matplotlib inline 
 
 import cucim.skimage as skimage
 import skimage as skimagecpu
@@ -33,7 +31,7 @@ def get_seeds(image, xystep, zstep, sigma=5, x=30):
     
     # define a cube of x microns as a footprint
     # use the scale and floor division to find the number of pixels in each dimension to use
-    foot = cp.ones((int(x//planestep),
+    foot = cp.ones((int(x//zstep),
                int(x//xystep),
                int(x//xystep)))
     
@@ -55,7 +53,8 @@ def get_seeds(image, xystep, zstep, sigma=5, x=30):
     seedprops = skimage.measure.regionprops(localhigh_img, img)
     
     # preserve memory
-    del localhigh localhigh_img 
+    del localhigh 
+    dellocalhigh_img 
     
     # loop through he object and get the seeds into an array
     seedlist = []
@@ -170,3 +169,63 @@ def detect_cell_thresh(image, seedcoord, thresh):
     cellimg = skimagecpu.segmentation.flood(bin_img_cpu,floodseed)
     
     return np.array(cellimg).astype(bool)
+
+
+# iterative cell detection
+def detect_cell_iter(image, seedcoord, expandpix, vlow, vhigh):
+    
+    # setup a threshold for the iterating, start with a little less than Otsu
+    # this way it reduces the volume from a too large fit
+    thresh_iter = find_cell_thresh(image, seedcoord, expandpix)*0.6
+    void_mask = cp.zeros_like(image)
+    
+    # get the candidate cell mask
+    CCM = detect_cell_thresh(image, seedcoord, thresh_iter)
+    # count the number of pixels in the mask (volume)
+    vol = cp.count_nonzero(cp.asarray(CCM))
+    
+    n_tries = 1
+    # if the volume is within the tolerance, return the mask
+    if (vol < vhigh and vol >vlow):
+        #print("...done in one go")
+        return CCM
+    
+    # while the number of pixels is outside the tolerance
+    while not(vol < vhigh and vol >vlow):
+        # if the volume is larger than target interval set threshold to previous*1.x
+        if vol > vhigh:
+            #print("too large")
+            thresh_iter = thresh_iter*1.2
+            n_tries = n_tries + 1
+            CCM = detect_cell_thresh(image, seedcoord, thresh_iter)
+            #update volume
+            vol = cp.count_nonzero(cp.asarray(CCM))
+            
+        # if the volume is below target interval set threshold to previous*0.x
+        if vol < vlow:
+            #print("too small")
+            thresh_iter = thresh_iter*0.8
+            n_tries = n_tries + 1
+            CCM = detect_cell_thresh(image, seedcoord, thresh_iter)
+            #update volume
+            vol = cp.count_nonzero(cp.asarray(CCM))
+            
+        # if the number of iterations is high and the cellmask is tiny than an absolute minimum, break and return empty mask
+        if (vol < vlow and n_tries > 6):
+            #print("Bad seed: Just a specle")
+            return void_mask.get()
+        
+        # if the number of iterations is high and the cell mask is massive, the seed is on the bg, break and return empty mask
+        if (vol > vhigh*3 and n_tries > 4):
+            #print("Bad seed: bg pixel, memory used: ", getGPUmem())
+            return void_mask.get()
+        
+        # if a reasonable volume is found return it
+        if (vol < vhigh and vol >vlow):
+            #print("...Found mask in", n_tries, "iterations")
+            return CCM
+        
+        # if no solution is found (bouncing right between to high and too low)
+        if (n_tries > 6):
+            #print("...Bad seed: Cant find a solution")
+            return void_mask.get()      
